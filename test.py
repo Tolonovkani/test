e17918d56e60e259a9f834f62c2e734afdbd3f70
Tolonovkani <tolonovkanybek072@gmail.com>
2023-11-15 16:33:47 +0600

    Внесли лекцию по Гиту

---
+++ test.py
@@ -0,0 +1,27 @@
+
+
+# git init 
+# git add
+# git commint -m "comment"
+# git remote git@git  копируюте и вставляти при создании нового репозитория  
+# git breach -M main
+# git push origin main 
+
+
+# после каждог обучения 
+# git  add.
+# git commint -m 'comment' 
+# git push origin main
+# 
+# git branch  все ветки
+# git branch <название новой ветки> создания новой ветки 
+# gti checkout <название ветки> пероход на ветку
+# git branch -D <название ветки> удаления ветки
+
+# git merge <название другой ветки> слияни текущей ветки с названой веткой
+
+# git clone <сылка на репозитроий> кланирования и скачивания данного репозитория 
+
+#  git push origin mmain отправка изменении
+#  git pull стиягивания изменении
+
